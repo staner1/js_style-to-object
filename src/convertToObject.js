31378b57164b5980976f0f4bacca113c58490a8f
Oleksandr <kozyrev.sania@gmail.com>
2025-04-03 20:43:29 +0300

    add solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,79 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  let arrayElements = sourceString.split(';');
+
+  arrayElements = arrayElements
+    .filter((item) => item.includes(':'))
+    .reduce((prev, item) => {
+      const brakeIndex = item.indexOf(':');
+
+      const keyResult = item
+        .slice(0, brakeIndex)
+        .replaceAll(' ', '')
+        .replaceAll('\n', '')
+        .replaceAll('\t', '');
+
+      const valueResult = () => {
+        const result = item.slice(brakeIndex + 1, item.length);
+
+        let arrayLetters = result.split('');
+
+        if (arrayLetters[0] === '\n') {
+          arrayLetters.splice(0, 1);
+        }
+
+        let newResult = '';
+
+        if (!arrayLetters.includes(',')) {
+          arrayLetters = arrayLetters
+            .join('')
+            .replaceAll('\n', '')
+            .replaceAll('"', '')
+            .replaceAll('\t', '')
+            .split('');
+
+          for (let i = 0; i < arrayLetters.length; i++) {
+            if (i === 0 && arrayLetters[i] === ' ') {
+              arrayLetters.splice(i, 1);
+              i--;
+              continue;
+            }
+
+            if (i === arrayLetters.length - 1 && arrayLetters[i] === ' ') {
+              newResult = newResult.slice(0, -1);
+              arrayLetters.splice(i, 1);
+              i -= 2;
+              continue;
+            }
+
+            newResult += arrayLetters[i];
+          }
+        }
+
+        if (arrayLetters.includes(',')) {
+          for (let i = 0; i < arrayLetters.length; i++) {
+            if (i === 0 && arrayLetters[i] === ' ') {
+              arrayLetters.splice(i, 1);
+              i--;
+              continue;
+            }
+
+            newResult = arrayLetters.slice(0).join('');
+            break;
+          }
+        }
+
+        return newResult;
+      };
+
+      return {
+        ...prev,
+        [keyResult]: `${valueResult()}`,
+      };
+    }, {});
+
+  return arrayElements;
 }
 
 module.exports = convertToObject;
